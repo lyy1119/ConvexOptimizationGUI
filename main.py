a276a6f11d75c6776add6dde18cc23edbe273e2f
lyy1119 <lyy2286301015@126.com>
2025-04-19 11:32:15 +0800

    202504191132

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PyQt6.QtGui import QAction
 from enum import Enum
 from PyQt6.QtCore import Qt, QThread, pyqtSignal
 
+from dialog import *
+
 defaultInputText = """函数：
 初始点： 
 epsilon x: 
@@ -369,56 +371,6 @@ class MainWidget(QWidget):
             #inputInfo += f"\nmethod："
             #inputInfo += f"{}"
             self.inputInfo.setText(inputInfo)
-
-class ProblemSwitch(QDialog):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.create_layout()
-        self.result = ProblemType.oneDimension
-
-    def create_layout(self):
-        self.setWindowTitle('选择优化模型类型')
-        self.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        # 创建布局
-        layout = QVBoxLayout(self)
-        # 一个单选框加按钮
-        group_box = QGroupBox("优化模型类型")
-        group_layout = QVBoxLayout()
-        # 创建单选框
-        radio1 = QRadioButton("一维无约束凸优化（指定方向的优化）")
-        radio2 = QRadioButton("多维无约束凸优化")
-        radio3 = QRadioButton("多维约束优化")
-        radio4 = QRadioButton("多目标多维约束优化")
-        # 默认选项
-        self.radios = {
-            radio1:ProblemType.oneDimension,
-            radio2:ProblemType.multiDimension,
-            radio3:ProblemType.constrainted, 
-            radio4:ProblemType.mutiTarget
-            }
-        radio1.setChecked(True)
-        group_layout.addWidget(radio1)
-        group_layout.addWidget(radio2)
-        group_layout.addWidget(radio3)
-        group_layout.addWidget(radio4)
-
-        group_box.setLayout(group_layout)
-        layout.addWidget(group_box)
-        # 确认与取消按钮
-        buttonLayout = QHBoxLayout()
-        layout.addLayout(buttonLayout)
-        ok = QPushButton("确认")
-        cancel = QPushButton("取消")
-        buttonLayout.addWidget(ok)
-        buttonLayout.addWidget(cancel)
-        ok.clicked.connect(self.accept)
-        cancel.clicked.connect(self.reject)
-
-    def accept(self):
-        for key, value in self.radios.items():
-            if key.isChecked():
-                self.result = value
-        return super().accept()
 
 class MainWindow(QMainWindow):
     def __init__(self):
